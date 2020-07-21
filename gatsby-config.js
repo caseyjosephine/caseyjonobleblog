c21547c6c1622599b435e8dd303f18ef6cf43450
Casey Noble <caseynoble@Starkiller-Base.local>
2020-07-20 19:07:04 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
+    title: `Casey Jo Noble`,
     name: `Narative`,
     siteUrl: `https://novela.narative.co`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Writing at the intersection of technology and the wine industry.`,
       maxWidth: 652,
     },
     social: [
